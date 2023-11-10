9c3b9d8fe284c83166dce84be11224c8d474209e
Bad Code <80211467+badxcode@users.noreply.github.com>
2023-11-10 21:25:19 +0600

    database connection

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $db = 'blog_web';
+
+    $config = mysqli_connect($host, $user, $password, $db);
+
+    // if(!$config)
+    // {
+    //     echo 'connection failed!';
+    // }
+    // else 
+    // {
+    //     echo 'connection successful!';
+    // }   
+?>
